fa50ec659b6bddbd7089684df54557a2624d6c56
Hiusu <luis_vala_junior@hotmail.com>
2022-09-04 22:40:18 -0300

    created css to cards lib

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
-class Player {
-    
+function loadGame() {
+    const grid = document.querySelector('.grid');
+    let card = new Card(1, "Test Name", "type", "./lib/cards/img/luffy.jpeg", "./lib/cards/img/luffy.jpg", "Test description");
 
-    constructor(name, life, gold, arrow, bow=new Cards()) {
-        this.name = name;
-        this.life = life;
-        this.gold = gold;
-        this.arrow = arrow;
-    }
+    card.action = () => {
+        console.log('hi');
+    };
+
+    grid.appendChild(card.toHTML());
+    grid.appendChild(card.toHTML());
+    grid.appendChild(card.toHTML());
+    grid.appendChild(card.toHTML());
+    grid.appendChild(card.toHTML());
+
+    let element = document.createElement("div");
+    grid.appendChild(element);
+}
+
+window.onload = () => {
+    loadGame();
 }

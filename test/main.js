5505352d6a0b2eb737aba38e1f6954658b978db2
Hiusu <luis_vala_junior@hotmail.com>
2022-09-06 03:15:20 -0300

    created the lib paths and some minor fixing

--- test/main.js
+++ test/main.js
@@ -1,38 +1,31 @@
-function loadGame() {
-    const grid = document.querySelector('.grid');
-    let card_01 = new Card(1, "Name 01", "ty", "./test/img/luffy.jpeg", "./test/img/luffy.jpg", "Test1 description");
+function testLibCards() {
+    const grid = document.querySelector('.grid-cards');
+    let card_01 = new Card(1, "Name 01", "type-01", "./test/img/luffy.jpg", "./test/img/luffy.jpeg", "Description 01");
     card_01.action = () => {
-        console.log('hi');
+        console.log('Card 01 flipped');
     };
-    let card_02 = new Card(2, "Name 02", "ty", "./test/img/luffy.jpeg", "./test/img/luffy.jpg", "Test2 description");
+    let card_02 = new Card(2, "Name 02", "type-02", "./test/img/luffy.jpg", "./test/img/luffy.jpeg", "Description 02");
     card_02.action = () => {
-        console.log('hi');
+        console.log('Card 02 flipped');
     };
-    let card_03 = new Card(3, "Name 03", "ty", "./test/img/luffy.jpeg", "./test/img/luffy.jpg", "Test3 description");
+    let card_03 = new Card(3, "Name 03", "type-03", "./test/img/luffy.jpg", "./test/img/luffy.jpeg", "Description 03");
     card_03.action = () => {
-        console.log('hi');
-    };
-    let card_04 = new Card(4, "Name 04", "ty", "./test/img/luffy.jpeg", "./test/img/luffy.jpg", "Test4 description");
-    card_04.action = () => {
-        console.log('hi');
-    };
-    let card_05 = new Card(5, "Name 05", "ty", "./test/img/luffy.jpeg", "./test/img/luffy.jpg", "Test5 description");
-    card_05.action = () => {
-        console.log('hi');
-    };
-    let card_06 = new Card(6, "Name 06", "ty", "./test/img/luffy.jpeg", "./test/img/luffy.jpg", "Test6 description");
-    card_06.action = () => {
-        console.log('hi');
+        console.log('Card 03 flipped');
     };
 
     grid.appendChild(card_01.toHTML());
     grid.appendChild(card_02.toHTML());
     grid.appendChild(card_03.toHTML());
-    grid.appendChild(card_04.toHTML());
-    grid.appendChild(card_05.toHTML());
-    grid.appendChild(card_06.toHTML());
+}
+
+function testLibPaths() {
+    Path.createPath(16,3, 'no');
+    console.log(Path.rooms);
+    Path.createPath(9,3);
+    console.log(Path.rooms);
 }
 
 window.onload = () => {
-    loadGame();
+    testLibCards();
+    testLibPaths();
 }

5f23f55549fe91437870ba6215c2b94527f4dba1
Hiusu <luis_vala_junior@hotmail.com>
2022-09-12 22:34:22 -0300

    created lb battle

--- test/main.js
+++ test/main.js
@@ -27,7 +27,52 @@ function testLibPaths() {
     console.log(Path.rooms);
 }
 
+function testLibBattle() {
+    let player_01 = new Char(1, 30, 30, 59, 5, 5);
+    let player_02 = new Char(2, 40, 40, 15, 20);
+    let player_03 = new Char(3, 50, 50, 10, 10, 1);
+    let player_04 = new Char(4, 60, 60, 5);
+    Battle.addAlly(player_01);
+    Battle.addAlly(player_02);
+    Battle.addAlly(player_03);
+    Battle.addAlly(player_04);
+    console.table(Battle.allies);
+    
+    let enemy_01 = new Char(1, 30, 30, 15, 5, 5);
+    let enemy_02 = new Char(2, 40, 40, 15, 20);
+    let enemy_03 = new Char(3, 50, 50, 10, 10, 1);
+    let enemy_04 = new Char(4, 60, 60, 5);
+    Battle.addEnemy(enemy_01);
+    Battle.addEnemy(enemy_02);
+    Battle.addEnemy(enemy_03);
+    Battle.addEnemy(enemy_04);
+    console.table(Battle.enemies);
+
+    Battle.normalAttack(player_01, enemy_01);
+    Battle.normalAttack(player_02, enemy_02);
+    Battle.normalAttack(player_03, enemy_03);
+    Battle.normalAttack(player_04, enemy_04);
+
+    Battle.autoAttack(player_01);
+    Battle.updateTurn();
+    
+    Battle.autoAttack(enemy_01);
+    Battle.updateTurn();
+
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+    Battle.autoAttack(player_01);
+}
+
 window.onload = () => {
     testLibCards();
-    testLibPaths();
+    testLibBattle();
 }
